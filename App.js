c174c5f853b0a45228d6b21efae87926eb740d31
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-20 16:03:57 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <a href="/">Inicio</a>
+                    <a href="/catalog">Catálogo</a>
+                    <a href="/contact">Contacto</a>
+                </nav>
+            </header>
+            <main>
+                <h1>Bienvenido a SPA Tallados</h1>
+                {/* Aquí agregaremos componentes específicos para cada página más adelante */}
+            </main>
+        </div>
+    );
+}
+
+export default App;
