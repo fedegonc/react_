a550e58680dcb98981fc05087f711327ee32a86a
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-20 16:02:31 -0300

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Archivo opcional para estilos globales
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
